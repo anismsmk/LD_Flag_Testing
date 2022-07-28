9d70a81c7e03f5026900ed8e83aa3044275b5606
AnisM MSMK <104977547+anismsmk@users.noreply.github.com>
2022-07-28 13:33:48 +0000

    Print fix

--- rps.py
+++ rps.py
@@ -16,24 +16,24 @@ def rps():
   #set player to True
       player = input("Rock, Paper, Scissors?\n")
       if player == computer:
-          print("Tie!")
+          print("Tie! \n")
       elif player == "Rock":
           if computer == "Paper":
-              print("You lose!", computer, "covers", player)
+              print("You lose!", computer, "covers", player ,"\n")
           else:
-              print("You win!", player, "smashes", computer)
+              print("You win!", player, "smashes", computer ,"\n")
       elif player == "Paper":
           if computer == "Scissors":
-              print("You lose!", computer, "cut", player)
+              print("You lose!", computer, "cut", player ,"\n")
           else:
-              print("You win!", player, "covers", computer)
+              print("You win!", player, "covers", computer ,"\n")
       elif player == "Scissors":
           if computer == "Rock":
-              print("You lose...", computer, "smashes", player)
+              print("You lose...", computer, "smashes", player ,"\n")
           else:
-              print("You win!", player, "cut", computer)
+              print("You win!", player, "cut", computer ,"\n")
       else:
-          print("That's not a valid play. Check your spelling!")
+          print("That's not a valid play. Check your spelling! \n")
       #player was set to True, but we want it to be False so the loop continues
       player = False
       computer = t[randint(0,2)]
@@ -43,7 +43,7 @@ def rps():
 
 def rps_crazy():
   print(""""The rules are as follows:
-                  Scissors cuts Paper  
+                Scissors cuts Paper  
                 Paper covers Rock   
                 Rock crushes Lizard   
                 Lizard poisons Spock   
@@ -66,25 +66,27 @@ def rps_crazy():
     if player == computer:
         print("Tie :/")
     if(computer == "Scissors" and (player == "Paper" or player == "Lizard")):
-        print("BAZINGA!")
+        print("BAZINGA! \n")
     elif(computer == "Lizard" and (player == "Paper" or player == "Spock")):
-        print("BAZINGA!")
+        print("BAZINGA!\n")
     elif(computer == "Spock" and (player == "Rock" or player == "Scissors")):
-        print("BAZINGA!")
+        print("BAZINGA!\n")
     elif(computer == "Paper" and (player == "Rock" or player == "Spock")):
-        print("BAZINGA!")
+        print("BAZINGA!\n")
     elif(computer == "Rock" and (player == "Scissors" or player == "Lizard")):
-        print("BAZINGA!")
+        print("BAZINGA!\n")
     elif(player == "Scissors" and (computer == "Paper" or computer == "Lizard")):
-        print("YOU LOSE!")
+        print("YOU LOSE!\n")
     elif(player == "Lizard" and (computer == "Paper" or computer == "Spock")):
-        print("YOU LOSE!")
+        print("YOU LOSE!\n")
     elif(player == "Spock" and (computer == "Rock" or computer == "Scissors")):
-        print("YOU LOSE!")
+        print("YOU LOSE!\n")
     elif(player == "Paper" and (computer == "Rock" or computer == "Spock")):
-        print("YOU LOSE!")
+        print("YOU LOSE!\n")
     elif(player == "Rock" and (computer == "sSissors" or computer == "Lizard")):
-        print("YOU LOSE!")
+        print("YOU LOSE!\n")
+    else:
+        print("That's not a valid play. Check your spelling! \n")
     #player was set to True, but we want it to be False so the loop continues
     player = False
     computer = T[randint(0,4)]
